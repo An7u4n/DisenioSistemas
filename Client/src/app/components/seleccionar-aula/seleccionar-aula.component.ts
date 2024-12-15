bb956d8f261537d81c882e51806b82e453bc3492
Rulopwd40 <132414402+Rulopwd40@users.noreply.github.com>
2024-12-15 15:33:12 -0300

    Front seleccionar aula

--- Client/src/app/components/seleccionar-aula/seleccionar-aula.component.ts
+++ Client/src/app/components/seleccionar-aula/seleccionar-aula.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-seleccionar-aula',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './seleccionar-aula.component.css'
 })
 export class SeleccionarAulaComponent {
+cancelar() {
+throw new Error('Method not implemented.');
+}
+volver() {
+throw new Error('Method not implemented.');
+}
+registrarReserva() {
+throw new Error('Method not implemented.');
+}
+
+aulas = [
+  { id: 1, ubicacion: 'Edificio A, Piso 1', capacidad: 30 },
+  { id: 2, ubicacion: 'Edificio B, Piso 2', capacidad: 25 },
+  { id: 3, ubicacion: 'Edificio C, Piso 3', capacidad: 40 },
+  { id: 4, ubicacion: 'Edificio D, Piso 1', capacidad: 20 }
+];
+
+  //@Input() aulas = [];
+
+  dias = ['Lunes','Martes','Miercoles']
 
 }
